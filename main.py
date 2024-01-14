294ce4c039bb44af1062b1ffd033fe6b140362bb
stoneshik <strebilly@mail.ru>
2024-01-14 14:25:01 +0300

    набросал вычисление коэффициентов для линейной регрессии

--- main.py
+++ main.py
@@ -4,78 +4,75 @@ import matplotlib.pyplot as plt
 
 
 class LinearRegression:
+    def __init__(self, x_matrix, y_matrix):
+        """
+        x = [[1 x11 x12 ... x1p]
+             [1 x21 x12 ... x1p]
+             [.. .. ... ... ...]
+             [1 xn1 xn2 ... xnp]]
+        y = [y1 y2 ... yn] (это столбец)
+        """
+        self.x_matrix = x_matrix
+        self.y_matrix = y_matrix
+        self.b_vector = self.__calc_b_vector()  # вектор с найденными коэффициентами
+        self.y_fit = self.__fit()
+
+    def __calc_b_vector(self):
+        x_transpose = np.transpose(self.x_matrix)
+        return np.linalg.inv(x_transpose.dot(self.x_matrix)).dot(x_transpose).dot(self.y_matrix)
+
+    def __fit(self):
+        pass
+
+
+def normalize_column(column):
     """
-    y = ax + b
-    Y = mX + c
+    Используется мин макс нормализация результат приниамет значение от 0 до 100
     """
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.m = 0
-        self.c = 0
-
-    def build_model(self):
-        """
-        Строим модель
-        """
-        x_mean = np.mean(self.x)
-        y_mean = np.mean(self.y)
-
-        num = 0
-        den = 0
-        for i in range(len(self.x)):
-            num += (self.x[i] - x_mean) * (self.y[i] - y_mean)
-            den += (self.x[i] - x_mean) ** 2
-        self.m = num / den
-        self.c = y_mean - self.m * x_mean
-        print(self.m, self.c)
-
-    def make_predictions(self):
-        """
-        Делаем предсказания
-        """
-        y_pred = self.m * self.x + self.c
-        plt.scatter(self.x, self.y, color='royalblue')  # actual
-        plt.plot([min(self.x), max(self.x)], [min(y_pred), max(y_pred)], color='red')  # predicted
-        plt.xlabel("x")
-        plt.ylabel("y")
-        plt.show()
+    if column[0] == 'Yes' or column[0] == 'No':
+        to_numbers_func = np.vectorize(lambda x: 100.0 if x == 'Yes' else 0.0)
+        column = to_numbers_func(column)
+    column_min = column.min()
+    column_max = column.max()
+    normalize_func = np.vectorize(lambda x: (x - column_min) / (column_max - column_min) * 100)
+    return normalize_func(column)
 
 
-def first_case(data):
-    x = data.iloc[:, 1]
-    y = data.iloc[:, 5]
-    model = LinearRegression(x, y)
-    model.build_model()
-    model.make_predictions()
+def normalize_data(data):
+    num_rows = len(data)
+    num_columns = len(data.columns)
+    matrix = np.zeros((num_rows, num_columns))
+    for num_column, column in enumerate(data.columns):
+        matrix[:, num_column] = normalize_column(data[column].to_numpy())
+    return matrix
 
 
-def second_case(data):
-    x = data.iloc[:, 0]
-    y = data.iloc[:, 3]
-    model = LinearRegression(x, y)
-    model.build_model()
-    model.make_predictions()
-
-
-def third_case(data):
-    x = data.iloc[:, 0]
-    y = data.iloc[:, 4]
-    model = LinearRegression(x, y)
-    model.build_model()
-    model.make_predictions()
+def first(normalized_data):
+    num_rows = len(normalized_data)
+    x_matrix = np.ones((num_rows, 6))
+    x_matrix[:, 1] = normalized_data[:, 0]
+    x_matrix[:, 2] = normalized_data[:, 1]
+    x_matrix[:, 3] = normalized_data[:, 2]
+    x_matrix[:, 4] = normalized_data[:, 3]
+    x_matrix[:, 5] = normalized_data[:, 4]
+    y_matrix = normalized_data[:, 5]
+    print(x_matrix)
+    print('=======================')
+    print(y_matrix)
+    print('=======================')
+    linear_regression = LinearRegression(x_matrix, y_matrix)
+    print(linear_regression.b_vector)
 
 
 def main():
     plt.rcParams['figure.figsize'] = (12.0, 9.0)
     # Preprocessing Input data
     data = pd.read_csv('Student_Performance.csv')
-    print(data)
-    first_case(data)
-    second_case(data)
-    third_case(data)
+    print(normalize_data(data))
+    print('=======================')
+    normalized_data = normalize_data(data)
+    first(normalized_data)
 
 
 if __name__ == '__main__':
     main()
-
