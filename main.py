82bc24ddd5c5dcd1ac743dbe0033c3d3278f3361
stoneshik <strebilly@mail.ru>
2024-01-14 15:22:15 +0300

    вывод

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 class LinearRegression:
@@ -22,12 +21,15 @@ class LinearRegression:
         return np.linalg.inv(x_transpose.dot(self.x_matrix)).dot(x_transpose).dot(self.y_matrix)
 
     def __fit(self):
-        pass
+        y_fit = np.zeros(len(self.y_matrix))
+        for num_row, x_row in enumerate(self.x_matrix):
+            y_fit[num_row] = sum([x_value * self.b_vector[i] for i, x_value in enumerate(x_row)])
+        return y_fit
 
 
 def normalize_column(column):
     """
-    Используется мин макс нормализация результат приниамет значение от 0 до 100
+    Используется мин макс нормализация результат принимает значение от 0 до 100
     """
     if column[0] == 'Yes' or column[0] == 'No':
         to_numbers_func = np.vectorize(lambda x: 100.0 if x == 'Yes' else 0.0)
@@ -56,22 +58,62 @@ def first(normalized_data):
     x_matrix[:, 4] = normalized_data[:, 3]
     x_matrix[:, 5] = normalized_data[:, 4]
     y_matrix = normalized_data[:, 5]
-    print(x_matrix)
-    print('=======================')
-    print(y_matrix)
-    print('=======================')
     linear_regression = LinearRegression(x_matrix, y_matrix)
-    print(linear_regression.b_vector)
+    print("Модель №1")
+    print("Для поиска значения индекса производительности")
+    print(f"Вектор с найденными коэффициентами:\n{linear_regression.b_vector}")
+    print(f"Найденные значения y:\n{linear_regression.y_fit}\n")
+
+
+def second(normalized_data):
+    num_rows = len(normalized_data)
+    x_matrix = np.ones((num_rows, 6))
+    x_matrix[:, 1] = normalized_data[:, 1]
+    x_matrix[:, 2] = normalized_data[:, 2]
+    x_matrix[:, 3] = normalized_data[:, 3]
+    x_matrix[:, 4] = normalized_data[:, 4]
+    x_matrix[:, 5] = normalized_data[:, 5]
+    y_matrix = normalized_data[:, 0]
+    linear_regression = LinearRegression(x_matrix, y_matrix)
+    print("Модель №2")
+    print("Для поиска значения часов учебы")
+    print(f"Вектор с найденными коэффициентами:\n{linear_regression.b_vector}")
+    print(f"Найденные значения y:\n{linear_regression.y_fit}\n")
+
+
+def third(normalized_data):
+    num_rows = len(normalized_data)
+    x_matrix = np.ones((num_rows, 6))
+    x_matrix[:, 1] = normalized_data[:, 0]
+    x_matrix[:, 2] = normalized_data[:, 2]
+    x_matrix[:, 3] = normalized_data[:, 3]
+    x_matrix[:, 4] = normalized_data[:, 4]
+    x_matrix[:, 5] = normalized_data[:, 5]
+    y_matrix = normalized_data[:, 1]
+    linear_regression = LinearRegression(x_matrix, y_matrix)
+    print("Модель №3")
+    print("Для поиска значения предыдущей оценки")
+    print(f"Вектор с найденными коэффициентами:\n{linear_regression.b_vector}")
+    print(f"Найденные значения y:\n{linear_regression.y_fit}\n")
+
+
+def visualize_info_dataset(data):
+    num_rows = len(data)
+    num_columns = len(data.columns)
+    print("Информация о датасете")
+    print("Содержит столбцы: Часов обучения; Предыдущая оценка; Дополнительная активность; Часы сна; Образцы вопросов, отработанных на практике; Индекс производительности")
+    print(f"Количество строк: {num_rows}")
+    print(f"Количество столбцов: {num_columns}")
+    print()
 
 
 def main():
-    plt.rcParams['figure.figsize'] = (12.0, 9.0)
-    # Preprocessing Input data
     data = pd.read_csv('Student_Performance.csv')
-    print(normalize_data(data))
-    print('=======================')
+    visualize_info_dataset(data)
     normalized_data = normalize_data(data)
     first(normalized_data)
+    second(normalized_data)
+    third(normalized_data)
 
 
 if __name__ == '__main__':
